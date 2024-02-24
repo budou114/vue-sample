baf3314100060629369ff4d99cfec473005a06bc
budou114 <58349030+budou114@users.noreply.github.com>
2024-02-24 14:46:23 +0900

    34. v-on 処理を埋め込む

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -6,6 +6,10 @@ const price1 = 40000;
 const price2 = 20000;
 
 const url1 = "https://www.amazon.co.jp/VECELO-%E3%83%A9%E3%83%83%E3%82%AF%E4%BB%98%E3%81%8D%EF%BC%88A4%E5%AF%BE%E5%BF%9C%EF%BC%89%E3%83%A9%E3%82%B9%E3%83%86%E3%82%A3%E3%83%83%E3%82%AF%E3%83%96%E3%83%A9%E3%82%A6%E3%83%B3-%E5%B9%85120%C3%97%E5%A5%A5%E8%A1%8C60cm-%E3%80%90%E6%97%A5%E6%9C%AC%E6%AD%A3%E8%A6%8F%E8%BC%B8%E5%85%A5%E5%93%81%E3%80%91-DX-T01-02/dp/B08P2RZWQP/ref=sr_1_1?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&crid=E5SBURPHDBC&dib=eyJ2IjoiMSJ9.iQP5v9kWZxukHSFwiDwqlQfOYsp7628yF1wn_nle3dHlr5X4akUfTdXaZsClGi-DrE-tAuShyreZnC7sayNwKr3R2Cpt_Sf6a98G93xLbQkVNp60QurTIaG3o-r-1ONJ2nYwFDQIQZMZc7Pm25Y9i5cEcl67ZxeFrQ-igdl62G9V9N-vVbECT4xHBnw_YyMXYY_x5Den2FVzc0yP0r08cFbANgyqULq-A7ScKrIzw3K8L8X07QJr2fVh0CsIoDiYSAZeeYTIQFzlyxBuzZ1FX1Mek9oH_UoIF_Nrt0gErAE.5SPcjhh1kIrPTCZyYVONHkAqmJCVGYt-JRDHV36un4w&dib_tag=se&keywords=desk&qid=1708753143&sprefix=desk%2Caps%2C308&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1"
+
+const buy = (itemName: string) => {
+  alert('Are you sure to buy' + itemName + "?");
+}
 </script>
 
 <template>
@@ -15,10 +19,12 @@ const url1 = "https://www.amazon.co.jp/VECELO-%E3%83%A9%E3%83%83%E3%82%AF%E4%BB%
       <label>{{ item1 }}</label>
       <label>{{ price1 }} yen</label>
       <a v-bind:href=url1> bought at...</a>
+      <button v-on:click="buy(item1)">BUY</button>
     </div>
     <div class="payment">
       <label>{{ item2 }}</label>
       <label>{{ price2 }} yen</label>
+      <button v-on:click="buy(item2)">BUY</button>
     </div>
   </div>
 </template>
